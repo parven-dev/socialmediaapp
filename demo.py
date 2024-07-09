ef273dd5cfc0a99c4f088b14cb7fa0ab4edeee13
ryan <39787691+ryan-cu1200@users.noreply.github.com>
2024-07-09 21:42:24 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,6 +3,6 @@ import random
 
 otp = "".join([str(random.randint(0, 9)) for _ in range(6)])
 
-email = ["ryan.cu1200@gmail.com"]
+email = ["user@gmail.com"]
 sender_mail = email[0]
 print(sender_mail)
